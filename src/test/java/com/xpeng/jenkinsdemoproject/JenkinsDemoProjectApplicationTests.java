3a69a96836e88bb61f5ee082518382ece57d611e
Ethanw <99559494+Shawnond@users.noreply.github.com>
2023-11-30 10:03:18 -0500

    next

---
+++ src/test/java/com/xpeng/jenkinsdemoproject/JenkinsDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpeng.jenkinsdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
